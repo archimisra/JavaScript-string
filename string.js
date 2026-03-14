4c91b66e6f786b9d06aa333f0a695bf655716c12
Archi Misra <misraarchi117@gmail.com>
2026-03-15 00:01:05 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,14 @@
+let obj = {
+    item: "pen",
+    price:  20,
+};
+console.log("the cost of", obj.item,"is",obj.price);
+//template literals
+let specialString = 'this is a template literal';
+console.log(typeof specialString);
+
+let str = "Archi Misra";
+str.toUpperCase();
+console.log(str);
+
+//strings are immutable in JavaScript 
